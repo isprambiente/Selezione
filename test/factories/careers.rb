8a54bbfb8007659c205ee21a98270899eb517e94
MDreW <andrea.ranaldi@gmail.com>
2022-08-24 21:42:55 +0200

    Add career model test

---
+++ test/factories/careers.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :career do
+    request 
+    employer { "MyEmployer" }
+    category { "td" }
+    description { "MyDescription" }
+    start_on { Date.today - 1.year }
+    stop_on { Date.today - 6.month }
+  end
+end
---
+++ test/models/career_test.rb
@@ -0,0 +1,83 @@
+require "test_helper"
+
+class CareerTest < ActiveSupport::TestCase
+  test 'valid from factory' do
+    career = build :career
+    assert career.valid?
+    assert career.save
+  end
+
+  # relations
+  test 'belongs to request' do
+    career = create :career
+    assert_equal 'Request', career.request.class.name
+  end
+
+  # Validation
+  test 'request must be present' do
+    career = build :career, request: nil
+    assert_not career.valid?
+    career.request = create :request
+    assert career.valid?
+    assert career.save
+  end
+  test 'employer must be present' do
+    career = build :career, employer: nil
+    assert_not career.valid?
+    career.employer = 'employer'
+    assert career.valid?
+    assert career.save
+  end
+  test 'category must be present' do
+    career = build :career, category: ''
+    assert_not career.valid?
+    career.category = 'ti'
+    assert career.valid?
+    assert career.save
+  end
+  test 'description must be present' do
+    career = build :career, description: ''
+    assert_not career.valid?
+    career.description = 'description'
+    assert career.valid?
+    assert career.save
+  end
+  test 'start_on must be present' do
+    career = build :career, start_on: nil
+    assert_not career.valid?
+    career.start_on = Date.today - 1.year
+    assert career.valid?
+    assert career.save
+  end
+  test 'start_on must be minor of stop_on' do
+    career = build :career, start_on: Date.today.-(9.month), stop_on: Date.today.-(11.month)
+    assert_not career.valid?
+    career.start_on = Date.today - 12.month
+    assert career.valid?
+    assert career.save
+  end
+  test 'stop_on must be present' do
+    career = build :career, stop_on: nil
+    assert_not career.valid?
+    career.stop_on = Date.today
+    assert career.valid?
+    assert career.save
+  end
+  test 'stop_on must be minor or equal to stop_at' do
+    request = create :request
+    career = build :career, request: request, stop_on: request.stop_at + 1.days
+    assert_not career.valid?
+    career.stop_on = request.stop_at
+    assert career.valid?
+    career.stop_on = request.stop_at - 1.days
+    assert career.valid?
+    assert career.save
+  end
+  test 'length must be major of 14' do
+    career = build :career, start_on: Date.today - 16.days, stop_on: Date.today - 2.days
+    assert_not career.valid?
+    career.stop_on = Date.today - 1.days
+    assert career.valid?
+    assert career.save
+  end
+end
